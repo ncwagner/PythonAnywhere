e4609e8026c7ecead22f687b7fe3185f33863805
Noah Wagner <ncwagner@unomaha.edu>
2020-09-15 17:47:24 -0500

    CMS Clone

---
+++ users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomerUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'department', 'employee_cell_phone', 'is_staff']
+
+
+admin.site.register(CustomUser, CustomerUserAdmin)
